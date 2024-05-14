f6e4f2f4255e50962aa2d40e2e42439c7fa41ad8
Ahmed Bawazeer <96870301+Ahmed-Bawza@users.noreply.github.com>
2024-05-14 14:07:19 +0300

    Update MyTrAiner.py

--- MyTrAiner.py
+++ MyTrAiner.py
@@ -7,9 +7,9 @@ if (exercise == "none"):
     st.header("Please Choose an exercise")
 
 elif (exercise == "squat"):
-    video_file = open('IMG_6038.mp4', 'rb')
-    video_bytes = video_file.read()
-    st.video(video_bytes)
+    # video_file = open('IMG_6038.mp4', 'rb')
+    # video_bytes = video_file.read()
+    # st.video(video_bytes)
     url = 'https://wesam0001.github.io/MyTrainer/'
     st.page_link(url, label="ابدا تمرينك", icon="🏋🏼‍♂️")
 
